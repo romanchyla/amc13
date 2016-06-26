2b13aaf61b9e96d28f3cbb7c02ad32822249c3ad
Roman Chyla <roman.chyla@gmail.com>
2016-06-26 10:43:07 -0400

    Multiple ip/port/slots + proper logging

--- dev_tools/amct/cli/__init__.py
+++ dev_tools/amct/cli/__init__.py
@@ -2,9 +2,12 @@ import click
 import os
 import amct
 import json
+import tempfile
 from amct import utils, config
 from amct.firmware import cli as firmware_cli
 from amct.testsuite import cli as testsuite_cli
+import logging
+import logging.config
 
 """The main command line interface to AMCT; here we initialize and
 set global defaults. Each of the modules of the AMCT have their 
@@ -17,7 +20,8 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'],
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.option('-v', '--verbose', count=True)
 @click.option('-d', '--debug', default=False, is_flag=True)
-@click.option('-l', '--log-level', type=click.Choice(['info', 'debug']))
+@click.option('-l', '--log-level', default='ERROR',
+              type=click.Choice(['INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL']))
 @click.option('-c', '--config', type=click.Path(exists=True, 
                     readable=True), help="Global configuration",
                     default=lambda: os.path.join(os.path.dirname(amct.__file__), './config.py')
@@ -25,15 +29,29 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'],
 @click.version_option()
 @click.pass_context
 def cli(ctx, verbose, debug, log_level, config):
+    class MyDict(dict):
+        pass
     conf = utils.load_config(config)
-    if debug:
-        print 'Config loaded from {0}'.format(config)
-        print json.dumps(conf, indent=2)
     conf.setdefault('CONFIG', debug)
     conf.setdefault('VERBOSE', verbose)
+    
+    
     conf.setdefault('LOG_LEVEL', log_level)
     
+    log_config = conf.get('LOGGING', {})
+    if verbose or debug:
+        log_level = debug and 'DEBUG' or 'INFO'
+        for k,v in log_config.get('handlers', {}).items():
+            v['level'] = log_level
+    logging.config.dictConfig(log_config)
+    
+    conf = MyDict(conf)
+    conf.logger = logging.getLogger(__name__)
+    
     ctx.obj = conf
+    if debug:
+        conf.logger.debug('Config loaded from {0}'.format(config))
+        conf.logger.debug(json.dumps(conf, indent=2))
 
 # expose nested sections
 cli.add_command(firmware_cli.cli)
--- dev_tools/amct/testsuite/cli.py
+++ dev_tools/amct/testsuite/cli.py
@@ -1,47 +1,69 @@
 """For running set of tests against AMC13."""
 
 import click
-import tabulate
-from amct import utils, config
+from amct import config
 from amct.testsuite.runner import AMCStandardRunner
 from amct.firmware.utils import get_ip_from_slot
 from tabulate import tabulate
-G = {}
+import os
 
 @click.group(name='testsuite')
 @click.pass_obj
 def cli(config):
     """Run predefined test-suites against AMC13 boards."""
-    G.update(config)
+    pass
 
 
 @cli.command()
-@click.option('-n', '--name', default=config.DEFAULT_TEST_SUITE)
-@click.option('-i', '--ip', default=None)
-@click.option('-x', '--port', default=None)
-@click.option('-s', '--slot', default=None, type=int)
-def run(name, ip, port, slot):
+@click.pass_obj
+def list(ctx):
+    """List the `ROOT/testsuites` folder."""
+    tests = []
+    i = 0
+    for x in os.listdir(ctx.get('DEFAULT_TESTSUITE_DIR')):
+        if os.path.isdir(os.path.join(ctx.get('DEFAULT_TESTSUITE_DIR'), x)):
+            readme = os.path.join(ctx.get('DEFAULT_TESTSUITE_DIR'), x, 'readme.txt')
+            if os.path.exists(readme):
+                with open(readme, 'r') as f:
+                    desc = f.read().strip()
+            else:
+                desc = ''
+            tests.append(('%02d' % i, x, desc))
+            i += 1
+    tests = sorted(tests, key=lambda x: x[1])
+    ctx.logger.info('Listing testsuites from: {0}'.format(ctx.get('DEFAULT_TESTSUITE_DIR')))
+    print tabulate(tests, headers=['#', 'name', 'description'])
+
+
+@cli.command()
+@click.option('-n', '--name', default=config.DEFAULT_TEST_SUITE,
+              help="Name of the suite to execute, eg. hello")
+@click.option('-i', '--ip', default=None,
+              help='IP address of the AMC card. Can be a comma separated list.')
+@click.option('-x', '--port', default=None,
+              help='Port of the AMC card. Can be a comma separated list.')
+@click.option('-s', '--slot', default=None,
+              help='Slots of the AMC card. Can be a comma separated list.')
+@click.pass_obj
+def run(ctx, name, ip, port, slot):
     """Run test-suite against AMC13. The tests are to be found
     inside `ROOT/testsuites` folder."""
-    #G['DEBUG']= True
-    if slot:
-        hexIP,decIP = get_ip_from_slot(config.DEFAULT_CRATE_IP, slot)
-        ip = decIP
-    print ip, port,
+    
+    if slot: # translate slots into IPs
+        ip = []
+        for x in slot.split(','):
+            _, dec_ip = get_ip_from_slot(ctx.get('DEFAULT_CRATE_IP'), int(x))
+            ip.append(dec_ip)
+    
+    if not ip and not port:
+        raise Exception('At least one of --ip or --port or --slot must be used')
+    
     # the idea is to create a test runner, it will discover all
     # files from a dedicated folder; those files should be amc
     # commands
-    runner = AMCStandardRunner(G, name, ip, port)
+    runner = AMCStandardRunner(ctx, name, ip=ip, port=port)
     runner.run()
-    #log = open("deneme.txt", "w")
-    #print >>log, runner.results
-    #print runner.results
-    print tabulate(runner.results[0])
-    #print tabulate.tabulate(runner.results[0])
+    
     # if we got here, it means the test was successful
-    if not G.get('verbose'):
-        runner.cleanup()
-        print 'Testsuite {0} finished OK.'
-    else:
-        print 'Testsuite {0} finished OK, results can be found in: {1}'.format(name, runner.tmpdir)
+    ctx.logger.info('Testsuite {0} finished OK'.format(name))
     
