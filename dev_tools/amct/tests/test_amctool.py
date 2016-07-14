9cd8540623638f765a51417a18a5a039f2ec2bcf
Roman Chyla <roman.chyla@gmail.com>
2016-07-13 21:37:18 -0400

    Finished the part that evaluated amc test scripts

--- dev_tools/amct/tests/test_amctool.py
+++ dev_tools/amct/tests/test_amctool.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 import unittest
 import mock
 import json
+import traceback
 from amct.amctool.commandline import AMCTool
 
 class TestAMCTool(TestCase):
@@ -28,7 +29,39 @@ class TestAMCTool(TestCase):
         self.assertEquals(str(mockeroo.call_args_list[0]),
         "call('AMCToolX -c 1 -c 2 -X {0}/amc/00_init.amc', None)".format(self.testsuite_dir))
 
+    @mock.patch.object(AMCTool, 'run')
+    def test_eval(self, mockeroo):
+        code = """
+for x in range(3):
+    v = amc.run(x)
+    print x, "{FOO}", v
+    """
+        mockeroo.return_value = 'ok'
+        runner = AMCTool('AMCTool13.exe', 1)
+        out = runner.eval_code(code, 'foo.amc', {'FOO': 'bar'})
+        assert out.splitlines() == ['0 bar ok', '1 bar ok', '2 bar ok']
+        
+        # check the amc.run() was called 3 times and what command it actually got
+        assert mockeroo.call_count == 3
+        assert str(mockeroo.call_args_list) == '[call(0), call(1), call(2)]'
+        
     
+    @mock.patch.object(AMCTool, 'run')
+    def test_eval_error(self, mockeroo):
+        """Evaluating syntactically incorrect code."""
+        code = """
+for x in range(3):
+    v = amc.run(x)
+    print x, "{FOO}, v
+    """
+        mockeroo.return_value = 'ok'
+        runner = AMCTool('AMCTool13.exe', 1)
+        try:
+            out = runner.eval_code(code, 'foo.amc', {'FOO': 'bar'})
+        except SyntaxError, e:
+            exc = traceback.format_exc()
+            assert 'File "foo.amc", line 4\n    print x, "bar, v\n                   ^' in exc
+            assert str(e) == 'EOL while scanning string literal (foo.amc, line 4)'
 
 if __name__ == '__main__':
     unittest.main() 
